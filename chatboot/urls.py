80595a24b59352ac7f04e069722ef2b0c5183da8
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-11-03 02:32:21 +0600

    successfully implemented the get and fetch request. user can send the message and get a basic answer

--- chatboot/urls.py
+++ chatboot/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("django_chatboot.urls")),
+    path('', include("django_chatboot.urls")),
 ]
--- django_chatboot/views.py
+++ django_chatboot/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
-def index(request):
+def chatboot(request):
+    print(request.method)
+    if request.method == 'POST':
+        message = request.POST.get('message') 
+        response = 'I am fine. What about you?'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatboot.html')
