c165dfcbddb22eb06e0fec96b0880242182ec7cd
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-11-03 16:48:49 +0600

    impleted the user login, registration and logout founctionality

--- django_chatboot/urls.py
+++ django_chatboot/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
 from . import views
 
+
 urlpatterns = [
     path('', views.chatboot, name="chatboot"),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
--- django_chatboot/views.py
+++ django_chatboot/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
 import os
 from dotenv import load_dotenv
+from django.contrib import auth 
+from django.contrib.auth.models import User
 
 
 load_dotenv()
@@ -23,9 +25,54 @@ def ask_openai(question):
     answer = response.choices[0].text.lstrip()
     return answer
 
+
 def chatboot(request):
     if request.method == 'POST':
         message = request.POST.get('message') 
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatboot.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatboot')
+        else:
+            error_message = 'Invalid user!'
+            return render(request, 'login.html', {'error_message': error_message})
+        
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1) 
+                user.save()
+                auth.login(request, user)
+                return redirect('chatboot')
+            except:
+                error_message = "Error creating account"
+                return render(request, 'register.html', {'error_message': error_message})
+        else:
+            error_message = "Password does not match!"
+            return render(request, 'register.html', {'error_message': error_message})
+        
+    return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
