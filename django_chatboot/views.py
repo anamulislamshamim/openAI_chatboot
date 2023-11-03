ecd694430984a2f04047aba0b1843dffce3d941b
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-11-03 06:24:30 +0600

    successfully integrate openai api

--- django_chatboot/views.py
+++ django_chatboot/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
 
+
+open_api_key = "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"
+openai.api_key = open_api_key
+
+
+def ask_openai(question):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = question,
+        max_tokens = 150,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+    )
+    print(response)
+    answer = response.choices[0].text.lstrip()
+    return answer
 
 def chatboot(request):
-    print(request.method)
     if request.method == 'POST':
         message = request.POST.get('message') 
-        response = 'I am fine. What about you?'
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatboot.html')
